8601bf35cb821bbae46688631fd4d90dc6420cc8
Brian Bargh <Brian@Brians-MacBook-Pro.local>
2015-10-15 16:47:43 -0700

    Modify sieve to be more space efficient

--- lib/sieve.py
+++ lib/sieve.py
@@ -1,23 +1,28 @@
 class Sieve:
-  'For telling which numbers are prime and which are composite.'
-  'Construction is O(nlog(log(n))), lookups constant time thereafter.'
-  'Memory used is O(n)'
+    'For telling which numbers are prime and which are composite.'
+    'Construction is O(nlog(log(n))), lookups constant time thereafter.'
+    'Memory used is O(n)'
 
-  def isPrime(self, k):
-    return self.sieve[k]
+    def __init__(self, n):
+        # array of n booleans initalized to true
+        self.sieve = [True] * (n/2)
+        # 1 is not prime
+        self.sieve[1/2] = False
+        for i in range(3, n, 2):
+            # If i is prime, mark all the multiples of i as composite
+            if self.sieve[i/2]:
+                # 2 * i is even so we already know that's composite
+                tmp = 3 * i
+                while tmp < n:
+                    self.sieve[tmp/2] = False
+                    # tmp + i is even, advance to next odd multiple of i
+                    tmp += 2 * i
 
-  def __init__(self, n):
-
-    # array of n booleans initalized to true
-    self.sieve = [True] * n
-
-    # 0 and 1 are not prime
-    self.sieve[0] = False
-    self.sieve[1] = False
-
-    for i in range(2, n):
-      if self.sieve[i]:
-        tmp = 2 * i
-        while tmp < n:
-          self.sieve[tmp] = False
-          tmp += i
+    def isPrime(self, k):
+        if k % 2 == 0:
+            if k == 2:
+                return True
+            else:
+                return False
+        else:
+            return self.sieve[k/2]
--- lib/test_sieve.py
+++ lib/test_sieve.py
@@ -7,6 +7,8 @@ def test_sieve():
     s = Sieve(100)
     n.assert_equal(s.isPrime(0), False)
     n.assert_equal(s.isPrime(1), False)
+    n.assert_equal(s.isPrime(2), True)
+    n.assert_equal(s.isPrime(4), False)
     n.assert_equal(s.isPrime(9), False)
     n.assert_equal(s.isPrime(2), True)
     n.assert_equal(s.isPrime(7), True)
